f144d2aaa9ca880754c41a51148c53ca8977206a
kangalioo <jannik.a.schaper@web.de>
2021-07-28 11:21:05 +0200

    Use poise built-in slash register code

--- src/commands/other.rs
+++ src/commands/other.rs
@@ -14,36 +14,7 @@ pub async fn ping(ctx: Context<'_>) -> CommandResult {
 /// Run with no arguments to register in guild, run with argument "global" to register globally.
 #[poise::command(hide_in_help)]
 pub async fn register(ctx: PrefixContext<'_>, #[flag] global: bool) -> CommandResult {
-    let guild = ctx
-        .msg
-        .guild(ctx.discord)
-        .await
-        .ok_or("Must be called in guild")?;
-
-    if ctx.msg.author.id != guild.owner_id {
-        return Err("Can only be used by server owner".into());
-    }
-
-    let mut commands_builder = poise::serenity_prelude::CreateApplicationCommands::default();
-    let commands = &ctx.framework.options().slash_options.commands;
-    for cmd in commands {
-        commands_builder.create_application_command(|f| cmd.create(f));
-    }
-
-    poise::say_prefix_reply(ctx, format!("Registering {} commands...", commands.len())).await?;
-    let json_value = serde_json::Value::Array(commands_builder.0);
-    if global {
-        ctx.discord
-            .http
-            .create_global_application_commands(&json_value)
-            .await?;
-    } else {
-        ctx.discord
-            .http
-            .create_guild_application_commands(guild.id.0, &json_value)
-            .await?;
-    }
-    poise::say_prefix_reply(ctx, "Done!".to_owned()).await?;
+    poise::defaults::register_slash_commands(ctx, global).await?;
 
     Ok(())
 }
